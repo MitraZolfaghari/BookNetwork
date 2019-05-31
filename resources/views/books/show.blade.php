26e2c073c7784cc6cee3538f3fcea935ead65fad
Mitra Zolfaghari <zolfaghari.mitra@gmail.com>
2019-05-31 18:43:45 +0430

    add container class

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container">
     <h1>{{$book->name}}</h1>
     <p><b>Pages:</b> {{$book->pages}}</p>
     <p><b>ISBN:</b> {{$book->isbn}}</p>
     <p><b>Price:</b> {{$book->price}} T</p>
     <p><b>Paublished at:</b> {{$book->publishedat}}</p>
-
+</div>
 @endsection
